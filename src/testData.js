e27102858cfd3b2a4498ecbfb76d89dcd65276d8
EkinKar <ekin1260@hotmail.com>
2023-03-06 00:25:41 +0100

    added recharts

---
+++ src/testData.js
@@ -0,0 +1,50 @@
+export const userData = [
+  {
+    name: "Jan",
+    "Active User": 1000,
+  },
+  {
+    name: "Feb",
+    "Active User": 3000,
+  },
+  {
+    name: "Mar",
+    "Active User": 2000,
+  },
+  {
+    name: "Apr",
+    "Active User": 2780,
+  },
+  {
+    name: "May",
+    "Active User": 1890,
+  },
+  {
+    name: "Jun",
+    "Active User": 2390,
+  },
+  {
+    name: "Jul",
+    "Active User": 3490,
+  },
+  {
+    name: "Aug",
+    "Active User": 2110,
+  },
+  {
+    name: "Sep",
+    "Active User": 2490,
+  },
+  {
+    name: "Oct",
+    "Active User": 1490,
+  },
+  {
+    name: "Nov",
+    "Active User": 4290,
+  },
+  {
+    name: "Dec",
+    "Active User": 3490,
+  },
+];
