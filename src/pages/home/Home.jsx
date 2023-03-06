7f5b1eb392177714d85915a030bfed19b78575f4
EkinKar <ekin1260@hotmail.com>
2023-03-06 21:35:21 +0100

    added widgets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,12 +3,18 @@ import Chart from "../../components/chart/Chart";
 import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
 import "./home.css";
 import { userData } from "../../testData";
+import SmallWidget from "../../components/smallWidget/SmallWidget";
+import LargeWidget from "../../components/largeWidget/LargeWidget";
 
 const Home = () => {
   return (
     <div className="home">
       <FeaturedInfo />
       <Chart data={userData} title="User Analytics" grid dataKey="name" />
+      <div className="homeWidget">
+        <SmallWidget />
+        <LargeWidget />
+      </div>
     </div>
   );
 };
